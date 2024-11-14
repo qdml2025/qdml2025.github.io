16369f8407f870a2d59829e8bf3e2f82330b25e5
Tim Littau <113526482+tmlittau@users.noreply.github.com>
2024-11-14 09:51:51 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
